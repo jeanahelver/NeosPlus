60643989a22b241e10c387ae65934a4aeb0854d8
jeana <jeana.helver@tuta.io>
2022-09-27 18:40:07 +0200

    idk testing something

---
+++ Components/Utility/DynScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BaseX;
+using FrooxEngine.LogiX;
+using FrooxEngine.UIX;
+using Jint;
+namespace FrooxEngine
+{
+	//very experiemental 
+	[Category("Data/Dynamic")]
+	public class DynScript : Component
+	{
+		public readonly Sync<string> code;
+
+		[ImpulseTarget]
+		public void execute()
+		{
+			var engine = new Jint.Engine();
+			engine.Execute(code.Value);
+			engine.Invoke("update");
+		}
+	}
+}
